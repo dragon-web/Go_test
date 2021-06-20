0f5902b597f28c644473bb617b73b882adab8f28
dragon-web <8946815+dragon-web@user.noreply.gitee.com>
2021-06-20 10:09:26 +0800

    var

---
+++ Go/day_6_20/var.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+    "fmt"
+)
+
+func main(){
+    a := 10;
+    var aa,bb = 10,"stringtest"
+    fmt.Printf("type of a = %T value a = %d\n", a , a);
+   // fmt.Printf("type of aa = %T value a = %d\n", aa , a);
+    fmt.Println("aa = \n",aa);
+  //  fmt.Printf("type of bb = %T" ,bb,"value",bb);
+}
